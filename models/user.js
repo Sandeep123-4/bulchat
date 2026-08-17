0addde43fbd734a2ba10e09670c28e8c75564234
Sandeep123-4 <xtry122@gmail.com>
2026-08-17 08:33:12 +0545

    Update website

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     password: String,
     premium: {
         type: Boolean,
-        default: false
+        default:true
     },
     avatar: {
         type: String,
